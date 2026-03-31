c351cdbe2fcda025b5eb3e81c95c524babd084e9
irises2000 <irises2000211@gmail.com>
2026-03-31 22:56:15 +0900

    modified:   script.js

--- script.js
+++ script.js
@@ -695,7 +695,7 @@ class ManeStrand {
 
     ctx.lineWidth = 2.5;
     ctx.lineCap = "round";
-    ctx.shadowBlur = 5;
+    ctx.shadowBlur = 8;
     ctx.shadowColor = this.rootColor;
     ctx.stroke();
 
