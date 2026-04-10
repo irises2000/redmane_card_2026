250799d993e92d1971aa62bc254654266c00d576
irises2000 <irises2000211@gmail.com>
2026-04-11 00:06:08 +0900

    modified:   script.js

--- script.js
+++ script.js
@@ -401,13 +401,13 @@ class ManeStrand {
 
     const t = index / Math.max(total - 1, 1);
 
-    const hue = 0;
-    const saturation = 90 - t * 80;
-    const lightness = 52 + t * 38;
-    const rootLightness = 70 + t * 24;
+    // const hue = 0;
+    // const saturation = 90 - t * 80;
+    // const lightness = 52 + t * 38;
+    // const rootLightness = 70 + t * 24;
 
-    this.color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-    this.rootColor = `hsl(${hue}, ${Math.max(saturation - 10, 5)}%, ${rootLightness}%)`;
+    // this.color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+    // this.rootColor = `hsl(${hue}, ${Math.max(saturation - 10, 5)}%, ${rootLightness}%)`;
 
     for (let i = 0; i <= this.segments; i++) {
       const px = x + i * this.segmentLength * this.dirX;
@@ -585,7 +585,7 @@ class ManeStrand {
     ctx.strokeStyle = this.color;
     ctx.lineWidth = 2.5;
     ctx.lineCap = "round";
-    ctx.shadowBlur = 8;
+    ctx.shadowBlur = 7;
     ctx.shadowColor = this.rootColor;
     ctx.stroke();
 
